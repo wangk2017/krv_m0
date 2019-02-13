7b0a2df049fa875dcf455c8b30c65e11f7de51c0
wangk2017 <kittyjx_2017@163.com>
2019-02-13 15:01:00 +0800

    update debug information for simulation

--- tb/sim_inc/tb_defines.vh
+++ tb/sim_inc/tb_defines.vh
@@ -1,8 +1,8 @@
 //`define RISCV
 //`define ZEPHYR
-//`define ZEPHYR_PHIL
+`define ZEPHYR_PHIL
 //`define ZEPHYR_SYNC
-`define DHRYSTONE
+//`define DHRYSTONE
 //`define INT_TEST
 //`define PG_TEST
 
--- tb/zephyr_sync_debug.v
+++ tb/zephyr_sync_debug.v
@@ -1,44 +1,5 @@
-reg main_start;
-reg init_start;
-reg prepC_start;
-reg sched_init_start;
-reg setup_new_thread_start;
-reg add_to_ready_start;
-reg bg_to_main_start;
-reg mem_addr_hit0;
-reg mem_addr_hit1;
-reg mem_addr_hit2;
-reg mem_addr_hit4;
 wire uart_tx_wr = !(DUT.m_apb.uart_0.uart_0.WEn || DUT.m_apb.uart_0.uart_0.csn);
 wire[7:0] uart_tx_data = DUT.m_apb.uart_0.uart_0.data_in;
-
-wire [31:0] mem_addr = DUT.u_core.u_dmem_ctrl.mem_addr;
-wire mem_st = DUT.u_core.u_dmem_ctrl.store_mem;
-wire st_data = DUT.u_core.u_dmem_ctrl.store_data_mem;
-
-always @(posedge cpu_clk)
-begin
-	if((mem_addr==32'h40000) && mem_st)
-	begin
-		$display ("write to mem 40000");
-		$display ("@time %t  !",$time);
-		$display ("write data = %h",st_data);
-		$display ("\n");
-	end
-end
-
-always @(posedge cpu_clk)
-begin
-	if((mem_addr==32'h40090) && mem_st)
-	begin
-		$display ("write to mem 40090");
-		$display ("@time %t  !",$time);
-		$display ("write data = %h",st_data);
-		$display ("\n");
-	end
-end
-
-
 
 //Play a trick to let the simulation run faster
 
@@ -56,19 +17,24 @@ end
 
 wire test_end1;
 assign test_end1 = dec_pc == 32'h0000df8;
-//assign test_end1 = 0;
 
 integer fp_z;
 
 initial
 begin
-	$display ("zephyr sync\n");
+	$display ("=============================================\n");
+	$display ("running Zephyr OS application synchronization\n");
+	$display ("=============================================\n");
+
 	fp_z =$fopen ("./out/uart_tx_data_sync.txt","w");
 @(posedge test_end1)
 begin
 	$fclose(fp_z);
+	$display ("=============================================\n");
 	$display ("TEST_END\n");
-	$display ("Print data is stored in out/uart_tx_data_sync.txt\n");
+	$display ("The application Print data is stored in \n");
+	$display ("out/uart_tx_data_sync.txt\n");
+	$display ("=============================================\n");
 	$stop;
 end
 end
@@ -77,9 +43,10 @@ always @(posedge cpu_clk)
 begin
 	if(uart_tx_wr)
 		begin
-			$display ("UART Transmitt");
-			$display ("UART TX_DATA is %h \n",uart_tx_data);
 			$fwrite(fp_z, "%s", uart_tx_data);
+			$display ("UART Transmitt DATA is %s ",uart_tx_data);
+			$display ("@time %t  !",$time);
+			$display ("\n");
 		end
 
 end
@@ -91,41 +58,15 @@ parameter THREADB		= 32'h00000448;
 parameter HL			= 32'h000003c8;
 parameter RESCHEDULE		= 32'h0000013c;
 
-wire [31:0] mret_addr = DUT.u_core.u_fetch.mepc;
-wire [31:0] mret_instr = DUT.u_core.u_fetch.mret;
-
-always @ (posedge mret_instr)
+//application process trace during simulation
+always @(posedge cpu_clk)
 begin
-	
-	$display ("mret from pc_dec= %h\n", dec_pc);
-	$display ("@time %t  !",$time);
-	$display ("\n");
-
-end
-
-always @ (mret_addr)
-begin
-	$display ("mepc changed");
-	$display ("@time %t  !",$time);
-	$display ("\n");
-	$display ("mepc = %h \n",mret_addr);
-end
-
-always @(posedge cpu_clk or negedge cpu_rstn)
-begin
-	if(!cpu_rstn)
-	begin
-		main_start <= 1'b0;
-	end
-	else
-	begin
 		case (dec_pc)
 		MAIN:	//main
 		begin
 			$display ("Main Start");
 			$display ("@time %t  !",$time);
 			$display ("\n");
-			main_start <=  1'b1;
 		end
 		SWAP:// <__swap>
 		begin
@@ -164,6 +105,5 @@ begin
 			$display ("\n");
 		end
 	endcase
-	end
 end
 
